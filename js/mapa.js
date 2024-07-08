9cee7d938cb1f7cd040820f7d167a1d850c87db0
estevamnetof <estevam.neto07@gmail.com>
2024-07-08 04:51:45 -0300

    página de cinemas adicionada

--- js/mapa.js
+++ js/mapa.js
@@ -2,8 +2,6 @@ let h2 = document.querySelector('h2');
 var map;
 
 function success(pos) {
-    console.log(pos.coords.latitude, pos.coords.longitude)
-
     if (map === undefined) {
         map = L.map('mapid').setView([pos.coords.latitude, pos.coords.longitude], 13);
     } else {
@@ -16,17 +14,33 @@ function success(pos) {
     }).addTo(map);
 
     L.marker([pos.coords.latitude, pos.coords.longitude]).addTo(map)
-    .bindPopup('Eu estou aqui!')
-    .openPopup();
+        .bindPopup('Eu estou aqui!')
+        .openPopup();
+
+    var LeafIcon = L.Icon.extend({
+        options: {
+            iconSize: [38, 95],
+            iconAnchor: [22, 94],
+            popupAnchor: [-3, -76]
+        }
+    });
+
+    var firstIcon = new LeafIcon({iconUrl: '../img/icons-svg/pino-1.svg'}),
+        secondIcon = new LeafIcon({iconUrl: '../img/icons-svg/pino-2.svg'}),
+        thirdIcon = new LeafIcon({iconUrl: '../img/icons-svg/pino-3.svg'});
+
+    L.marker([-3.756702, -38.486017], {icon: firstIcon}).addTo(map).bindPopup("Iguatemi Bosque Fortaleza");
+    L.marker([-3.732948, -38.570544], {icon: secondIcon}).addTo(map).bindPopup("RioMar Kennedy Fortaleza");
+    L.marker([-3.76471, -38.57486], {icon: thirdIcon}).addTo(map).bindPopup("North Shopping Fortaleza");
 }
 
 function error(err) {
-    console.log(err)
+    console.log(`ERROR(${err.code}): ${err.message}`);
 }
 
 var watchID = navigator.geolocation.watchPosition(success, error, {
     enableHighAccuracy: true,
-    timeout: 5000
+    timeout: 10000,
+    maximumAge: 0
 });
 
-// navigator.geolocation.clearWatch(watchID);
